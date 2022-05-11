c1b3d80dc9c5577d4006c9a15ff704cb67428ef7
Leonardo Loureiro <Leonardo.Loureiro.2021@live.rhul.ac.uk>
2022-05-11 17:25:02 +0100

    init db will now start with their proper data types and defaults.

--- init_db.py
+++ init_db.py
@@ -26,9 +26,24 @@ connection.execute("""
   CREATE TABLE IF NOT EXISTS buggies (
     id                    INTEGER PRIMARY KEY,
     qty_wheels            INTEGER DEFAULT 4,
-    flag_color            VARCHAR(20),
-    flag_color_secondary  VARCHAR(20),
-    flag_pattern          VARCHAR(20)
+    power_type            VARCHAR(20) DEFAULT 'petrol',
+    power_units           INTEGER DEFAULT 1,
+    aux_power_type        VARCHAR(20) DEFAULT NULL,
+    aux_power_units       INTEGER DEFAULT 0,
+    hamster_booster       INTEGER DEFAULT 0,
+    flag_color            VARCHAR(20) DEFAULT 'plain',
+    flag_pattern          VARCHAR(20) DEFAULT 'plain',
+    flag_color_secondary  VARCHAR(20) DEFAULT 'black',
+    tyres                 VARCHAR(20) DEFAULT 'knobbly',
+    qty_tyres             INTEGER DEFAULT 4,
+    armour                VARCHAR(20) DEFAULT 'none',
+    attack                VARCHAR(20) DEFAULT 'none',
+    qty_attacks           INTEGER DEFAULT 0,
+    fireproof             BOOLEAN DEFAULT FALSE,
+    insulated             BOOLEAN DEFAULT FALSE,
+    antibiotic            BOOLEAN DEFAULT FALSE,
+    banging               BOOLEAN DEFAULT FALSE,
+    algo                  VARCHAR(20) DEFAULT 'steady',
   )
 
 """)
